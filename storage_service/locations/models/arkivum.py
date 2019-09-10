04215cf27fbea9dbe5094237c20e0c9d40538f45
Héctor Akamine <hakamine@artefactual.com>
2019-09-10 15:30:44 -0700

    Arkivum: make use of INSECURE_SKIP_VERIFY

--- storage_service/locations/models/arkivum.py
+++ storage_service/locations/models/arkivum.py
@@ -28,10 +28,7 @@ from .package import Package
 
 LOGGER = logging.getLogger(__name__)
 
-if settings.DEBUG:
-    VERIFY = False
-else:
-    VERIFY = True
+VERIFY = not settings.INSECURE_SKIP_VERIFY
 
 
 class Arkivum(models.Model):
